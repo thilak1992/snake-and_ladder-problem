d98e73f343d49da97104f45a12f89c41a1fda515
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-11-09 10:19:49 +0530

    Die rolls until player reaches winning position, 100

--- src/com/bridgelabz/SnakeAndLadderGame/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame/SnakeAndLadderGame.java
@@ -11,32 +11,35 @@ public class SnakeAndLadderGame {
             Random rand=new Random();
             int rollOutput,option;
 
-            rollOutput = rand.nextInt(6);
-            System.out.println("Die output : " + (rollOutput+1));
-            //checking option No play, Snake and Ladder
-            option=rand.nextInt(3);
 
-            switch (option){
-                case 0:
-                    System.out.println("No Play");
-                    playerPosition+=0;
-                    System.out.println(playerPosition);
-                    break;
-                case 1:
-                    System.out.println("Snake");
-                    playerPosition= playerPosition-rollOutput+1;
-                    if (playerPosition<0){
-                        playerPosition=0;
+            while (playerPosition<100) {
+                rollOutput = rand.nextInt(6);
+                System.out.println("Die output : " + (rollOutput+1));
+                //checking option No play, Snake and Ladder
+                option=rand.nextInt(3);
+
+                switch (option) {
+                    case 0:
+                        System.out.println("No Play");
+                        playerPosition += 0;
                         System.out.println(playerPosition);
-                    }else{
-                        System.out.println(playerPosition);}
-                    break;
-                case 2:
-                    System.out.println("Ladder");
-                    playerPosition+=rollOutput+1;
-                    System.out.println(playerPosition);
-                    break;
+                        break;
+                    case 1:
+                        System.out.println("Snake");
+                        playerPosition = playerPosition - rollOutput + 1;
+                        if (playerPosition < 0) {
+                            playerPosition = 0;
+                            System.out.println(playerPosition);
+                        } else {
+                            System.out.println(playerPosition);
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Ladder");
+                        playerPosition += rollOutput + 1;
+                        System.out.println(playerPosition);
+                        break;
+                }
             }
-
         }
 }
