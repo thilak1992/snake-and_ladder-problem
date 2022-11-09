861849cdfb438db12138d9a13cf200453b3457b2
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-11-09 10:13:30 +0530

    Player rolls and get a number between 1-6

--- src/com/bridgelabz/SnakeAndLadderGame/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame/SnakeAndLadderGame.java
@@ -7,7 +7,13 @@ public class SnakeAndLadderGame {
         public static void main(String[] args) {
             System.out.println("Welcome to Snake and Ladder Game ");
             int playerPosition=0;
-            System.out.println("Starting player position is "+playerPosition);
-
+            System.out.println("Starting player position is "+playerPosition+" ; Player rolls die");
+            Random rand=new Random();
+            int rollOutput;
+            rollOutput= rand.nextInt(6);
+            if (rollOutput ==0){
+                rollOutput =1;
+            }
+            System.out.println("Die output : "+rollOutput);
         }
 }
