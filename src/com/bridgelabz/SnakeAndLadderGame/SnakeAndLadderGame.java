18ecad7af9c112c9d6f812b8702d3a95407cd0e1
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-11-09 10:01:43 +0530

    Creating: creating master branch

---
+++ src/com/bridgelabz/SnakeAndLadderGame/SnakeAndLadderGame.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.SnakeAndLadderGame;
+
+import java.util.Random;
+
+public class SnakeAndLadderGame {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Snake and Ladder Game ");
+            int playerPosition=0;
+            System.out.println("Starting player position is "+playerPosition);
+
+        }
+}
